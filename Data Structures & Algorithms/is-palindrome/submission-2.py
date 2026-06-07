a41920e10d11fd93e3a799f6e4b2b504177b24cc
AnushLoknath <anushloknath@gmail.com>
2026-06-07 15:54:35 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        gg=""
+        for i in s:
+            if i.isalnum():
+                gg+=i.lower()
+        return gg==gg[::-1]        
+
+       
+
+
+
+
+       
