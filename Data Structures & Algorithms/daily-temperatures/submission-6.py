efad807231b8404cc306ee918a7bf848cd410326
AnushLoknath <anushloknath@gmail.com>
2026-06-15 07:11:35 +0530

    Add: daily-temperatures - submission-6

---
+++ Data Structures & Algorithms/daily-temperatures/submission-6.py
@@ -0,0 +1,19 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack=[]
+        ans=[0]*len(temperatures)
+        for i in range(len(temperatures)):
+            while stack and temperatures[i]>stack[-1][0]:
+                stacki,stackindex=stack.pop()
+                ans[stackindex]=i-stackindex
+            stack.append([temperatures[i],i])
+        return ans
+      
+
+
+
+
+
+
+
+        
