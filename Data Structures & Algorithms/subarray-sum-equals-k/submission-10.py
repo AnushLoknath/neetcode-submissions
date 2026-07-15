5cd9a1495a2edfc2eaf25e760e9d38ee23a5a5fe
AnushLoknath <anushloknath@gmail.com>
2026-07-15 13:03:50 +0530

    Add: subarray-sum-equals-k - submission-10

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-10.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        gg={0:1}
+        total=0
+        count=0
+        for i in nums:
+            total+=i
+            need=total-k
+            if need in gg:
+                count+=gg[need]
+            gg[total]=gg.get(total,0)+1
+        return count
+
+      
+
+        
