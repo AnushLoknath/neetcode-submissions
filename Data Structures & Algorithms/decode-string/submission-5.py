9fcad36818ceec003a73ac0981f402b5a84871d9
AnushLoknath <anushloknath@gmail.com>
2026-07-28 05:18:10 +0530

    Add: decode-string - submission-5

---
+++ Data Structures & Algorithms/decode-string/submission-5.py
@@ -0,0 +1,22 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack=[]
+        for i in s:
+            if i !="]":
+                stack.append(i)
+            else:
+                gg=""
+                while stack and stack[-1]!="[":
+                    gg=stack.pop()+gg
+                stack.pop()
+                
+                k=""
+                while stack and stack[-1].isdigit():
+                    k=stack.pop()+k
+                stack.append(gg*int(k))
+        return "".join (stack)
+                
+
+
+
+        
